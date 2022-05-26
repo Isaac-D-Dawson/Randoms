f0a5dde1b9322734676d6f62b87f50ab8ff6dff5
Isaac Dawson <little_tornado@hotmail.co.uk>
2022-05-26 15:31:34 +0100

    Updated to V1.2, Changelog within

--- Javascript/countDays.js
+++ Javascript/countDays.js
@@ -4,6 +4,7 @@
 //-Original Program structure and logic by Sharky Cuddle- program unchanged except for stylistic choices
 //V1.1:
 //-Corrected obvious mistakes so that program will actually run. Changes documented below.
+//-V1.2: Optimised program logic in an attempt to improve runtime.
 
 let daysInMonth = [31, 28, 31, 30, 31, 30, 31, 31, 30, 31, 30, 31];
 let totalDays   = 0;
@@ -11,20 +12,24 @@ let dayOfMonth  = prompt("Enter day of month");
 let monthOfYear = prompt("Enter month number");
 let year        = prompt("Enter Year");
 
-for (let i = 0; i < monthOfYear-1; i++) {
-    totalDays = totalDays + daysInMonth[i];
-}
+//Replaced for-loop with a reduce call on a list slice.
+totalDays = daysInMonth.slice(0, monthOfYear-1).reduce(function(current, total){
+    total = total + current
+    return total
+}, 0)
 
-totalDays = totalDays + parseInt(dayOfMonth);
+//Moved update and alert to end of program to ensure accurate final output
 
-alert(totalDays + " days have passed this year");
-
+//Streamlined isLeapYear function considerably
 function isLeapYear(targetYear) {
-    if (targetYear % 400 === 0) {return true};
-    if (targetYear % 100 === 0) {return false};
-    return targetYear % 4 === 0;
+    return ( targetYear % 400 === 0 || (targetYear % 4 === 0 && targetYear % 100 !== 0) );
 };
 
-if (isLeapYear(year) === true){
+//Trimmed excess "is true"
+if (isLeapYear(year)){
     totalDays = totalDays + 1;
 };
+
+totalDays = totalDays + parseInt(dayOfMonth);
+//Updated Alert to use a `format string`
+alert(`${totalDays} days have passed this year`);
