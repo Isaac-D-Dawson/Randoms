1c3203705a93146c3dcc797f32ee3b1eaf8a1b91
Isaac Dawson <little_tornado@hotmail.co.uk>
2022-06-08 13:05:45 +0100

    Updated file to V1.4, see internal changelog

--- Javascript/countDays.js
+++ Javascript/countDays.js
@@ -11,25 +11,25 @@
 //-Replaced uneeded function with a direct if-statement
 //-Removed reduce function by pre-processing the result set it created
 //-retyped output string for clarity
+//V1.4:
+//-Corrected underlying logic issue with Leap Years.
+//-Noted small discrepancy with parseInt- purely commentary, no actual change.
 
-//Replaced daysInMonth with daysInMonths
 let daysInMonths = [0, 31, 59, 90, 120, 151, 181, 212, 243, 273, 304, 334];
 let totalDays   = 0;
 let dayOfMonth  = prompt("Enter day of month");
 let monthOfYear = prompt("Enter month number");
 let year        = prompt("Enter Year");
 
-//Further replaced Reduce function(Formerly for-loop) by replacing the daysInMonth list with a
-// preprocessed "daysInMonths" list to ensure accurate final output
-//We now assign this pre-processed value directly to the totalDays var,
 totalDays = daysInMonths[monthOfYear -1];
 
-
-//Removed unneeded isLeapYear function- moved internal if statement directly into relevant conditional
-if (year % 400 === 0 || (year % 4 === 0 && year % 100 !== 0)) {
+//Added a month check to confirm we're only adding leap days AFTER Febuary, and not always.
+if ( monthOfYear > 2 && (year % 400 === 0 || (year % 4 === 0 && year % 100 !== 0))) {
     totalDays = totalDays + 1;
 };
 
-totalDays = totalDays + parseInt(dayOfMonth);
-//Updated Alert to use a, slightly clearer, `format string`
+totalDays = totalDays + parseInt(dayOfMonth);   //Wait, shouldn't this be different?
+//totalDays = totalDays + Number.parseInt(dayOfMonth, 10)
+//You could probably also use this to handle the concept of months to years in some kind of duodecimal system
+
 alert(`${totalDays} days have passed since your date in ${year}`);
